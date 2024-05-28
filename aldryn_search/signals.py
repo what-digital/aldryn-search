0fa53104ee568fe54fc09306e7ceabef0e09e531
dvanser <ivans.dorohovs@gmail.com>
2024-05-28 11:51:48 +0300

    fix Signal.__init__() got an unexpected keyword argument 'providing_args'

--- aldryn_search/signals.py
+++ aldryn_search/signals.py
@@ -7,5 +7,5 @@ Created on Nov 30, 2015
 from django.dispatch.dispatcher import Signal
 
 
-add_to_index = Signal(providing_args=['instance', 'object_action'])
-remove_from_index = Signal(providing_args=['instance', 'object_action'])
+add_to_index = Signal(['instance', 'object_action'])
+remove_from_index = Signal(['instance', 'object_action'])
